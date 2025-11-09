fa92dd7bbc6a92b2dd780ed0988d886e6251abf3
MizCODE <marcelbarriosmarchisio@gmail.com>
2025-11-09 11:48:56 -0300

    Update inserts.sql

--- config/scripts_db/inserts.sql
+++ config/scripts_db/inserts.sql
@@ -8,29 +8,6 @@ INSERT INTO Games (date_played, winner_id) VALUES
 -- Insertar jugadores por partida con puntaje inicial 0
 
 -- Partida 1
-INSERT INTO GamePlayers (game_id, user_id, score) VALUES
-(1, 1, 150),  -- Marcos
-(1, 2, 120),  -- Nacho
-(1, 3, 100),  -- Marcel
-(1, 4, 90),   -- Gabriel
-(1, 5, 80);   -- Luis
-
--- Partida 2
-INSERT INTO GamePlayers (game_id, user_id, score) VALUES
-(2, 1, 130),  -- Marcos
-(2, 2, 140),  -- Nacho
-(2, 3, 110),  -- Marcel
-(2, 4, 95),   -- Gabriel
-(2, 5, 85);   -- Luis
-
--- Partida 3
-INSERT INTO GamePlayers (game_id, user_id, score) VALUES
-(3, 1, 160),  -- Marcos
-(3, 2, 135),  -- Nacho
-(3, 3, 120),  -- Marcel
-(3, 4, 100),  -- Gabriel
-(3, 5, 90);   -- Luis
-
 -- Inserts Dinno-runner
 
 INSERT INTO `Dinosaurs` (`dino_id`, `species`, `weight`, `sprite_frame1`, `sprite_frame2`) VALUES
